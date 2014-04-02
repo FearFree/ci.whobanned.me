d92f84f7da91f7c7bd56de59c662e901ad2ad684
FearFree <FearFree@users.noreply.github.com>
2014-04-01 20:33:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,12 @@ function scan_dir($dir, &$files) {
     $files = array_keys($files);
 }
 
-scan_dir("../dev", $files);
-echo '<table><tr><td>Latest Release Build: </td><td><a href="../release/1.1">1.1</a></td></tr>';
-echo '<tr><td>Latest Dev Build: </td><td><a href="../dev/' . $files['0'] . '">' . $files['0'] . '</a></td><td><img src="https://drone.io/github.com/FearFree/WhoBannedMe/status.png"></td></tr></table>';
+scan_dir("/var/www/dev", $files);
+echo '<table><tr><td>Latest Release Build: </td><td><a href="http://ci.whobanned.me/release/1.1">1.1</a></td></tr>';
+echo '<tr><td>Latest Dev Build: </td><td><a href="http://ci.whobanned.me/dev/' . $files['0'] . '">' . $files['0'] . '</a></td><td><img src="https://drone.io/github.com/FearFree/WhoBannedMe/status.png"></td></tr></table>';
 echo '<u>Previous Versions</u><br>';
 foreach(array_slice($files, 1) as $build){
-  echo '<a href="dev/' . $build . '">' . $build . '</a><br>';
+  echo '<a href="http://ci.whobanned.me/dev/' . $build . '">' . $build . '</a><br>';
 }
 ?>
 </body>
