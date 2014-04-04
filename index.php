be8f92bca49a7eff6a9f67483437b17b3dfd1f11
Chris Everett <evo@clevo.net.nz>
2014-04-05 00:45:32 +1300

    Roll semantic UI and style elements and clean structured output.

--- index.php
+++ index.php
@@ -1,34 +1,51 @@
 <?php
-function scan_dir($dir, &$files) {
+
+  function scan_dir($dir, &$files) {
     $ignored = array('.', '..',);
 
     $files = array();
     foreach (scandir($dir) as $file) {
-        if (in_array($file, $ignored)) continue;
-        $files[$file] = filemtime($dir . '/' . $file);
+      if (in_array($file, $ignored))
+        continue;
+      $files[$file] = filemtime($dir . '/' . $file);
     }
 
     arsort($files);
-}
-scan_dir("/var/www/dev", $files);
-$filekeys = array_keys($files);
+  }
 
-echo '<html>
-<head>
-<title>WhoBannedMe</title>
-</head>
-<body>
-WhoBannedMe Development Artifacts<br>
-<table>
-    <tr><td>Current Release Build: </td><td><a href="http://ci.whobanned.me/release/1.1">1.1</a></td></tr>
-    <tr><td>Latest Dev Build: </td><td><a href="http://ci.whobanned.me/dev/' . $filekeys['0'] . '">' . $filekeys['0'] . '</a></td><td>' . gmdate("Y-m-d H:i:s", $files[$filekeys['0']]) . '<td><img src="https://drone.io/github.com/FearFree/WhoBannedMe/status.png"></td></tr>
-</table>
-<u>Previous Versions</u><br>
-<table>';
-foreach(array_slice($files, 1) as $build => $date){
-  echo '<tr><td><a href="http://ci.whobanned.me/dev/' . $build . '">' . $build . '</td><td>' . gmdate("Y-m-d H:i:s", $date) . '</a><br>';
-}
-echo '</table>
-</body>
-</html>';
+  scan_dir("/var/www/dev", $files);
+  $filekeys = array_keys($files);
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>WhoBannedMe</title>
+    <link rel="stylesheet" href="semantic-ui/css/semantic.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+  </head>
+  <body>
+    <h1 class="ui dividing header">WhoBannedMe Development Artifacts</h1>
+    
+    <p>Current Release Build: <a class="white" href="http://ci.whobanned.me/release/1.1">1.1</a></p>
+    
+    <p>Latest Dev Build: 
+      <a href="http://ci.whobanned.me/dev/<?php echo($filekeys['0']);?>"><?php echo($filekeys['0']); ?></a><?php //echo(gmdate("Y-m-d H:i:s", $files[$filekeys['0']])); ?>
+      <img src="https://drone.io/github.com/FearFree/WhoBannedMe/status.png">
+    </p>
+
+
+    
+    <h3 class="ui header">Previous Versions</h3>
+    
+    <div class="ui list">
+      <?php
+        foreach (array_slice($files, 1) as $build => $date) {
+          echo '<a class="item" href="http://ci.whobanned.me/dev/' . $build . '">' . $build . '' . gmdate("Y-m-d H:i:s", $date) . '</a>';
+        }
+      ?>
+    </div>
+  </body>
+</html>
